0580a04fd5809176951f750e2b96a53af1d4e94f
iamadityaanjana <iamadityaanjana@gmail.com>
2025-04-30 00:05:14 +0530

    added redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  async redirects() {
+    return [
+      {
+        source: '/packages/Solana',
+        destination: 'https://www.npmjs.com/package/solana-deploy-sdk',
+        permanent: true,
+      },
+      {
+        source: '/packages/Solana/',
+        destination: 'https://www.npmjs.com/package/solana-deploy-sdk',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
